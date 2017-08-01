c6bbcf2f5cfeb966be3f44998cc34c964eac1be4
mlazear10 <mlazear10@icloud.com>
2017-07-31 18:19:45 -0700

    still need to improve thetiming functions

--- app.js
+++ app.js
@@ -6,9 +6,16 @@
     projectId: "trainscheduler-8a419",
     storageBucket: "trainscheduler-8a419.appspot.com",
     messagingSenderId: "419390718535"
- };
- firebase.initializeApp(config);
+  };
+//Make sure the rules in the database are set to true 
+// {
+//   "rules": {
+//     ".read": "auth != true",
+//     ".write": "auth != true"
+//   }
+// }
 
+ firebase.initializeApp(config);
 
 // VARIABLES
 // --------------------------------------------------------------------------------
@@ -19,8 +26,16 @@ var train = {
 	destination: "",
 	frequency: "",
 	satrtTime: "",
-	minutesAway: ""
+	minutesAway: "",
+	nextArrival: ""
 };
+var firstTimeConverted = "";
+var currentTime = "";
+var diffTime = '';
+var tRemainder = "";
+var minutesTillTrain = '';
+var nextTrain = '';
+var nextTrainFormatted = '';
 
 
 console.log("app.js has started.")
@@ -33,18 +48,42 @@ $("#submitButton").on("click", function() {
 	train.destination = $("#destination").val().trim();
 	train.startTime = $("#startTime").val().trim();
 	train.frequency = $("#frequency").val().trim();
-	train.minutesAway = 10; //??????? neeed to calculate the minutes away reference the moment.js solution from day 21
+	train.minutesAway = train.startTime + train.frequency;
+	train.nextArrival = train.startTime;
 
-	console.log("train locally");
+	// if function to make sure all fields are selected
+	if(train.name == "" || train.destination == "" || train.startTime == "" || train.frequency == "") {
+		alert("Please fill out all fields before continuing");
 
-	// Change what is saved in firebase
-      database.ref().push({
-        name: train.name,
-        destination: train.destination,
-        startTime: train.startTime, 
-        frequency: train.frequency,
-        minutesAway: train.minutesAway
-      });
+	}
+
+	// Update time for the train to come t
+	// else if (train.startTime != moment(train.startTime).format('HHmm')) {
+	// 	alert('Please enter time in military format');
+	// 	$('#startTime').val('');
+	// }
+
+
+	// if all conditions are met then push to database
+	else {
+
+		console.log("train locally");
+
+		// Change what is saved in firebase
+	     database.ref().push({
+	        name: train.name,
+	        destination: train.destination,
+	        startTime: train.startTime, 
+	        frequency: train.frequency,
+	        nextArrival: train.nextArrival,
+	        minutesAway: train.minutesAway
+	     });
+
+	    $("#name").val("");
+	    $("#destination").val("");
+	    $("#startTime").val("");
+	    $("#frequency").val("");
+	}
 
 });
 
@@ -56,21 +95,52 @@ database.ref().on("child_added", function(snapshot) {
   $("#trainTable").append("<tr><td>" + 
   	snapshot.val().name + "</td><td>" + 
   	snapshot.val().destination + "</td><td>" + 
-  	snapshot.val().startTime + "</td><td>" + 
   	snapshot.val().frequency + "</td><td>" + 
+  	snapshot.val().nextArrival + "</td><td>" +
   	snapshot.val().minutesAway + "</td></tr>");
+
+// handle errors ask a question on this
+}, function(errorObject) {
+	// console.log("Errors handled: " + errorObject.code);
 });
 
-// }, function(errorObject) {
-//   console.log("The read failed: " + errorObject.code);
-// });
 
-// //Change the HTML to reflect
+// Set time variables
 
-// $("#name-display") = snapshot.val().employee.name
-// $("#role-display") = employee.role
-// $("#start-date-display") = employee.startDate
-// $("#months-worked-display") = 
-// $("#monthly-rate-display") = 
-// $("#total-billed-display") = 
+//
+// firstTimeConverted = moment(train.startTime, "HH:mm").subtract(1, "days");
+// console.log(firstTimeConverted);
+
+// currentTime = moment().format("HH:mm");
+// console.log(currentTime);
+
+// diffTime = moment().diff(moment.unix(train.startTime), "minutes");
+// console.log(diffTime);
+
+// tRemainder = diffTime % frequency;
+// minutesTillTrain = frequency - tRemainder;
+// nextTrain = moment().add(minutesTillTrain, "minutes");
+// nextTrainFormatted = moment(nextTrain).format("HH:mm");
+
+var diffTime = moment().diff(moment.unix(train.startTime), "minutes");
+var tRemainder = moment().diff(moment.unix(train.startTime), "minutes") % train.frequency ;
+var tMinutes = train.frequency - tRemainder;
+
+// To calculate the arrival time, add the tMinutes to the currrent time
+var tArrival = moment().add(tMinutes, "m").format("HH:mm"); 
+console.log(tMinutes);
+console.log(tArrival);
+
+console.log(moment().format("HH:mm"));
+console.log(tArrival);
+console.log(moment().format("X"));
+
+// neeed to setup an interval function to get the times working
+
+//Create a loop to find out when the next train is coming
+// for (i = firstTrain, i >= timeNow )
+
+
+
+
 
